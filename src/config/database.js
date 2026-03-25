82901e272af1d9aa04364c94652ff08aaa167dcd
lilia <liliahernandez2004@gmail.com>
2026-03-24 19:39:34 -0600

    Agregando IDs virtuales globales a Mongoose

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,11 @@
 const mongoose = require('mongoose');
 
+// Añadir virtual id automáticamente a todos los modelos (igual que PostgreSQL)
+mongoose.plugin(function(schema) {
+  schema.set('toJSON', { virtuals: true });
+  schema.set('toObject', { virtuals: true });
+});
+
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.DATABASE_URL || 'mongodb://127.0.0.1:27017/pokemon_app', {
